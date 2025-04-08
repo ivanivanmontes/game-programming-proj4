923f7482e679bd73d7163580d948487e1f9fe8b7
Ivan Montes <101290628+ivanivanmontes@users.noreply.github.com>
2025-04-08 12:36:48 -0400

    forgot to add header comment sorry

--- irm4019_proj4/Effects.cpp
+++ irm4019_proj4/Effects.cpp
@@ -1,3 +1,13 @@
+/**
+* Author: Ivan Reynoso Montes
+* Assignment: Rise of the AI
+* Date due: 2025-04-05, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #include "Effects.h"
 
 Effects::Effects(glm::mat4 projection_matrix, glm::mat4 view_matrix) :  m_current_effect(NONE), m_alpha(1.0f), m_effect_speed(1.0f), m_size(10.0f)
